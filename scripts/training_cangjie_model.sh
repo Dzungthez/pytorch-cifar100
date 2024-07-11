ed83ba272b182fe5bacc4f1f5faeea07e64f8b82
Dzzzung <huudung141434@gmail.com>
2024-07-11 22:43:12 +0700

    abcdefg

--- scripts/training_cangjie_model.sh
+++ scripts/training_cangjie_model.sh
@@ -1,1 +1,12 @@
 
+
+# export PYTHONPATH=$(pwd)
+PYTHONFILE='../cangjie/training.py'
+
+NET="resnet50" 
+GPU=1
+BATCH_SIZE=4
+WARMUP=1
+LEARNING_RATE=1e-4
+
+python $PYTHONFILE -net $NET -gpu $GPU -batch $BATCH_SIZE -warmup $WARMUP -lr $LEARNING_RATE
